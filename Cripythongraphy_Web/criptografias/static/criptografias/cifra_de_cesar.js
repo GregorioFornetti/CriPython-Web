af8f294d4ac87ba4853fdc78fb7126b0f678b2bd
GregorioFornetti <fornetti.greg@gmail.com>
2020-09-11 00:04:13 -0300

    tradução cifra de cesar

--- Cripythongraphy_Web/criptografias/static/criptografias/cifra_de_cesar.js
+++ Cripythongraphy_Web/criptografias/static/criptografias/cifra_de_cesar.js
@@ -3,46 +3,67 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('form').onsubmit = () => {
         const chave = document.querySelector('#chave').value;
         const mensagem = document.querySelector('#mensagem').value;
-        encriptar_cesar_apenas_letras(chave, mensagem)
+        encriptar_cesar_apenas_letras(chave, mensagem);
         return false;
     }
 })
 
 function encriptar_cesar_apenas_letras(chave, mensagem) {
-    chave = parseInt(chave) % 26 // Converter chave para inteiro e aplicar modulo de 26 (já que após 26, já deu uma volta).
-    let mensagem_encriptada = '';
+    chave = parseInt(chave) % 26;
+    console.log(criar_mensagem_cesar_apenas_letras(chave, mensagem));
+}
+
+function traduzir_cesar_apenas_letras(chave, mensagem) {
+    chave = 26 - parseInt(chave) % 26;
+    console.log(criar_mensagem_cesar_apenas_letras(chave, mensagem));
+}
+
+
+function criar_mensagem_cesar_apenas_letras(chave, mensagem) {
+    let mensagem_nova = '';
     if (!mensagem) {
-        alert('erro mensagem');
+        return "Erro mensagem !";
     }
     if (chave) {
         for (indice_caractere in mensagem) {
             const valor_unicode_atual = mensagem.charCodeAt(indice_caractere);
             let valor_unicode_somado = valor_unicode_atual + chave;
             if (valor_unicode_atual >= COMECO_UNICODE_MAIUSC && valor_unicode_atual <= FIM_UNICODE_MAIUSC) {
-                if (valor_unicode_somado > FIM_UNICODE_MAIUSC) {
+                if (valor_unicode_somado > FIM_UNICODE_MAIUSC) {  // Novo valor passou de "Z". Hora de voltar para o começo.
                     valor_unicode_somado -= 26;
                 }
-                mensagem_encriptada += String.fromCharCode(valor_unicode_somado);
+                mensagem_nova += String.fromCharCode(valor_unicode_somado);
             } else if (valor_unicode_atual >= COMECO_UNICODE_MINUSC && valor_unicode_atual <= FIM_UNICODE_MINUSC) {
-                if (valor_unicode_somado > FIM_UNICODE_MINUSC) {
+                if (valor_unicode_somado > FIM_UNICODE_MINUSC) {  // Novo valor passou de "z". Hora de voltar para o começo.
                     valor_unicode_somado -= 26;
                 }
-                mensagem_encriptada += String.fromCharCode(valor_unicode_somado);
+                mensagem_nova += String.fromCharCode(valor_unicode_somado);
             } else {
-                mensagem_encriptada += mensagem[indice_caractere];
+                mensagem_nova += mensagem[indice_caractere];
             }
         }
-        alert(mensagem_encriptada);
+        return mensagem_nova
     } else {
-        alert('erro chave');
+        return "Erro chave !";
     }
 }
 
+
 function encriptar_cesar_varios_caracteres(chave, mensagem) {
     chave = parseInt(chave) % tamanho_unicode_limitado;
-    let mensagem_encriptada = '';
+    console.log(criar_mensagem_cesar_varios_caracteres(chave, mensagem));
+}
+
+function traduzir_cesar_varios_caracteres(chave, mensagem) {
+    chave = tamanho_unicode_limitado - parseInt(chave) % tamanho_unicode_limitado;
+    console.log(criar_mensagem_cesar_varios_caracteres(chave, mensagem));
+}
+
+
+function criar_mensagem_cesar_varios_caracteres(chave, mensagem) {
+    let mensagem_nova = '';
     if (!mensagem) {
-        alert('erro mensagem');
+        return "Erro mensagem !";
     }
     if (chave) {
         for (indice_caractere in mensagem) {
@@ -54,19 +75,16 @@ function encriptar_cesar_varios_caracteres(chave, mensagem) {
                     caractere_atual = caractere_atual + 'str';
                 }
                 let valor_unicode_somado = JSON_unicode_limitado[caractere_atual] + chave;
-                console.log(valor_unicode_somado)
-                console.log(tamanho_unicode_limitado)
-                if (valor_unicode_somado > tamanho_unicode_limitado) {
+                if (valor_unicode_somado >= tamanho_unicode_limitado) {
                     valor_unicode_somado -= tamanho_unicode_limitado;
                 }
-                console.log(valor_unicode_somado)
-                mensagem_encriptada += JSON_unicode_limitado[valor_unicode_somado];
+                mensagem_nova += JSON_unicode_limitado[valor_unicode_somado];
             } else {
-                mensagem_encriptada += mensagem[indice_caractere];
+                mensagem_nova += mensagem[indice_caractere];
             }
         }
+        return mensagem_nova;
     } else {
-        alert('erro chave');
+       return "Erro chave !";
     }
-    console.log(mensagem_encriptada);
 }
